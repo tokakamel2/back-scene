5136bb73180d8ea4eaff79235d269ad90727c89c
toka <tokakamel2@gmail.com>
2020-09-23 13:41:57 +0200

    'query'

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-mongoose.connect('mongodb://localhost/backery')
+mongoose.connect('mongodb+srv://tokakamel2:<password>@cluster0.rr1bh.mongodb.net/<dbname>?retryWrites=true&w=majority')
     .then(()=>console.log('connected to DB'))
     .catch(err=>console.log('couldnt connect to DB'))
 
